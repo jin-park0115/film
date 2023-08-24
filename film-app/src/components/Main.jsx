142e34aac09753f25e5f794c9a4602a1417f184a
DESKTOP-JVEBI2D\JIN <parkjin0115@naver.com>
2023-08-24 23:50:35 +0900

    9

--- film-app/src/components/Main.jsx
+++ film-app/src/components/Main.jsx
@@ -1,7 +1,6 @@
 import { styled } from "styled-components";
 import { Link } from "react-router-dom";
 import MovieList from "./MovieList";
-import MoreMovie from "./MoreMovie";
 const Main = () => {
   return (
     <>
